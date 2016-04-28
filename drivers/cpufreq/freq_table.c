multirom-aries/hellspawn-N4

commit 5f08022f2e3e368e215e54388bd51038d4afebc6

cpufreq: Break out early when frequency equals target_freq
Many drivers keep frequencies in frequency table in ascending
or descending order. When governor tries to change to policy->min
or policy->max respectively then the cpufreq_frequency_table_target
could return on first iteration. This will save some iteration cycles.

So, break out early when a frequency in cpufreq_frequency_table
equals to target one.

Testing this during kernel compilation using ondemand governor
with a frequency table in ascending order, the
cpufreq_frequency_table_target returned early on the first
iteration at about 30% of times called.

Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

drivers/cpufreq/freq_table.c
@@ -134,9 +134,13 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 			continue;
 		if ((freq < policy->min) || (freq > policy->max))
 			continue;
+		if (freq == target_freq) {
+			optimal.driver_data = i;
+			break;
+		}
 		switch (relation) {
 		case CPUFREQ_RELATION_H:
-			if (freq <= target_freq) {
+			if (freq < target_freq) {
 				if (freq >= optimal.frequency) {
 					optimal.frequency = freq;
 					optimal.index = i;
@@ -149,7 +153,7 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 			}
 			break;
 		case CPUFREQ_RELATION_L:
-			if (freq >= target_freq) {
+			if (freq > target_freq) {
 				if (freq <= optimal.frequency) {
 					optimal.frequency = freq;
 					optimal.index = i;

commit 358e982ea3e4154efcc69e4edc87583c8b6ffeb8

cpufreq: fix compile error introduced by https://github.com/spezi77/hellspawn-N4/commit/5f08022f2e3e368e215e54388bd51038d4afebc6
Signed-off-by: spezi77 <spezi77@gmx.com>

drivers/cpufreq/freq_table.c
@@ -135,7 +135,7 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 		if ((freq < policy->min) || (freq > policy->max))
 			continue;
 		if (freq == target_freq) {
-			optimal.driver_data = i;
+			optimal.frequency = i;
 			break;
 		}
 		switch (relation) {